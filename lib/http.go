a97f22c2c537ebbc0228403606a8e03fe4a67a5c
neymarjimoh <jemohkunle2007@gmail.com>
2024-01-27 19:09:36 +0100

    avoid exposing apiclient keys

--- lib/http.go
+++ lib/http.go
@@ -10,23 +10,23 @@ import (
 
 // APIClient is a wrapper for making HTTP requests to the fastotp API.
 type APIClient struct {
-	BaseURL string
-	APIKey  string
-	Ctx     context.Context
+	baseURL string
+	apiKey  string
+	ctx     context.Context
 }
 
 // NewAPIClient creates a new instance of APIClient.
 func NewAPIClient(baseURL, apiKey string, ctx context.Context) *APIClient {
 	return &APIClient{
-		BaseURL: baseURL,
-		APIKey:  apiKey,
-		Ctx:     ctx,
+		baseURL: baseURL,
+		apiKey:  apiKey,
+		ctx:     ctx,
 	}
 }
 
 // Post sends a POST request to the specified endpoint with the given payload.
 func (c *APIClient) Post(endpoint string, payload interface{}) (*http.Response, error) {
-	url := c.BaseURL + endpoint
+	url := c.baseURL + endpoint
 
 	// Convert payload to JSON
 	payloadBytes, err := json.Marshal(payload)
@@ -34,13 +34,13 @@ func (c *APIClient) Post(endpoint string, payload interface{}) (*http.Response, 
 		return nil, err
 	}
 
-	req, err := http.NewRequestWithContext(c.Ctx, http.MethodPost, url, bytes.NewBuffer(payloadBytes))
+	req, err := http.NewRequestWithContext(c.ctx, http.MethodPost, url, bytes.NewBuffer(payloadBytes))
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("x-api-key", c.APIKey)
+	req.Header.Set("x-api-key", c.apiKey)
 
 	client := http.DefaultClient
 	return client.Do(req)
@@ -48,16 +48,16 @@ func (c *APIClient) Post(endpoint string, payload interface{}) (*http.Response, 
 
 // Get sends a GET request to the specified endpoint, appending id as a path parameter
 func (c *APIClient) Get(id string) (*http.Response, error) {
-	url := fmt.Sprintf("%s/%s", c.BaseURL, id)
+	url := fmt.Sprintf("%s/%s", c.baseURL, id)
 	fmt.Println(url)
 
-	req, err := http.NewRequestWithContext(c.Ctx, http.MethodGet, url, nil)
+	req, err := http.NewRequestWithContext(c.ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("x-api-key", c.APIKey)
+	req.Header.Set("x-api-key", c.apiKey)
 
 	client := http.DefaultClient
 	return client.Do(req)
